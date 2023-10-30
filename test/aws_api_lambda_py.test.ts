9037d93e74cf343ce36cca42c94c345612ca3748
EmiRoberti77 <emiliano.roberti@accenture.com>
2023-10-30 07:39:14 +0000

    Initial commit

---
+++ test/aws_api_lambda_py.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as AwsApiLambdaPy from '../lib/aws_api_lambda_py-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/aws_api_lambda_py-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new AwsApiLambdaPy.AwsApiLambdaPyStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
